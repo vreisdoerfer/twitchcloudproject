5be786dec16c330110d9c11744e809bca8ae888e
vreisdoerfer <31447490+vreisdoerfer@users.noreply.github.com>
2021-10-02 20:21:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port 3000");
 });
 
