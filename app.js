565d3d839111400f4eea7df1d15d2416bc6e930d
Vitor R <vitorreisdoerfer@hotmail.com>
2021-10-03 12:38:26 -0300

    Comment to default route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port 3000");
 });
 
+//DEFAULT ROUTE
 app.get("", (req, res, next) => {
     var characters = [`Keqing`,`Benny Boi`,`Zhongli`,`Ganyu`,`Diluc`,`Paimon`,` Albedo`,`Aloy`,`Barbara`,`Beidou`,`Xiao`,`Jean`,`Lisa`,`Mona`,`Noelle`,`Qiqi`,`Sayu`,`Sucrose`,`Venti`];
     res.send(characters[Math.floor(Math.random() * characters.length)]);
